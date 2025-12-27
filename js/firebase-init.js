88c223fda821d1a53bfd4374c539a187140fcbaf
SiameseJames22 <robloxworld607@gmail.com>
2025-12-27 15:18:42 +0000

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,16 @@
+// js/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAexuq5Q8KV6xMC0AKVk7H0dnMFsjyPWes",
+  authDomain: "dworld-b10ea.firebaseapp.com",
+  projectId: "dworld-b10ea",
+  storageBucket: "dworld-b10ea.firebasestorage.app",
+  messagingSenderId: "893085470416",
+  appId: "1:893085470416:web:fe733dc6ca67380f70d8d5",
+  measurementId: "G-DVC13ZYNS1"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

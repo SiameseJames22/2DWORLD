92c42b4166162ec5871a77e88cac25b4acd8415f
SiameseJames22 <robloxworld607@gmail.com>
2025-12-30 00:22:14 +0000

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
-// firebase.js (ESM)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+// firebase.js (2DWORLD)
+// Safe to commit. Your project is protected by Auth + Firestore rules.
 
-export const firebaseConfig = {
-  "apiKey": "AIzaSyAexuq5Q8KV6xMC0AKVk7H0dnMFsjyPWes",
-  "authDomain": "dworld-b10ea.firebaseapp.com",
-  "projectId": "dworld-b10ea",
-  "storageBucket": "dworld-b10ea.firebasestorage.app",
-  "messagingSenderId": "893085470416",
-  "appId": "1:893085470416:web:fe733dc6ca67380f70d8d5",
-  "measurementId": "G-DVC13ZYNS1"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAexuq5Q8KV6xMC0AKVk7H0dnMFsjyPWes",
+  authDomain: "dworld-b10ea.firebaseapp.com",
+  projectId: "dworld-b10ea",
+  storageBucket: "dworld-b10ea.firebasestorage.app",
+  messagingSenderId: "893085470416",
+  appId: "1:893085470416:web:fe733dc6ca67380f70d8d5",
+  measurementId: "G-DVC13ZYNS1"
 };
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
